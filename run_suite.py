c415bc6b10215784b1356009a68cbd25325af98a
hxy <h18832672908@163.com>
2019-12-10 18:08:48 +0800

    1

--- run_suite.py
+++ run_suite.py
@@ -6,3 +6,5 @@ suite = unittest.defaultTestLoader.discover("./scripts")
 # 获取报告存储文件流 并 实例化HtmlTestRunner 调用run方法
 with open("./report/report.html","wb")as f:
     HTMLTestRunner(stream=f).run(suite)
+
+
